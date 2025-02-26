0a19117216e6604cefa6ac3db8329b32f0d0b275
Kadon Boldt <kadonboldt@u.boisestate.edu>
2025-02-25 19:43:20 -0700

    Added all function declarations

--- src/lab.c
+++ src/lab.c
@@ -3,6 +3,38 @@
 #include <unistd.h>
 #include "lab.h"
 
+char *get_prompt(const char *env) {
+    return '\0';
+}
+
+int change_dir(char **dir) {
+    return 0
+}
+
+char **cmd_parse(char const *line) {
+    return '\0';
+}
+
+void cmd_free(char ** line) {
+    return;
+}
+
+char *trim_white(char *line) {
+    return '\0';
+}
+
+bool do_builtin(struct shell *sh, char **argv) {
+    return false;
+}
+
+void sh_init(struct shell *sh) {
+    return;
+}
+
+void sh_destroy(struct shell *sh) {
+    return;
+}
+
 void parse_args(int argc, char **argv) {
     while ((int opt = getopt(argc, argv, "v")) != -1) {
         switch (opt) {
@@ -15,3 +47,4 @@ void parse_args(int argc, char **argv) {
         }
     }
 }
+
