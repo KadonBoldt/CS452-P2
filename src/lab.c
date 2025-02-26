b746916d01038a064093cfdb1289bc86e39b75b4
Kadon Boldt <kadonboldt@u.boisestate.edu>
2025-02-25 19:36:59 -0700

    Task 1 complete

--- src/lab.c
+++ src/lab.c
@@ -1,1 +1,17 @@
-/**Update this file with the starter code**/
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "lab.h"
+
+void parse_args(int argc, char **argv) {
+    while ((int opt = getopt(argc, argv, "v")) != -1) {
+        switch (opt) {
+            case 'v':
+                printf("Shell Version: %d.%d\n", lab_VERSION_MAJOR, lab_VERSION_MINOR);
+            exit(EXIT_SUCCESS);
+            default:
+                fprintf(stderr, "Usage: %s [-v]\n", argv[0]);
+            exit(EXIT_FAILURE);
+        }
+    }
+}
